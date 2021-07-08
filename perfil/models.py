878a46033d4d43986796c7e4c00324bca24705dc
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-07-08 09:32:14 -0300

    Mensagens de erro para campos adicionadas

--- perfil/models.py
+++ perfil/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ValidationError
+
+import re
+from utils.validacpf import valida_cpf
 
 # Create your models here.
 class Perfil(models.Model):
@@ -48,10 +52,21 @@ class Perfil(models.Model):
     )
 
     def __str__(self):
-        return f'{self.usuario.first_name} {self.usuario.last_name}'
+        return f'{self.usuario}'
 
     def clean(self):
-        pass
+        # Criando um dicionario para adicionar as mensagens, para no final levantarmos as exceções
+        error_messages = {}
+
+        if not valida_cpf(self.cpf):
+            error_messages['cpf'] = 'Digite um CPF válido!'
+        
+        # Expressão regualar
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            error_messages['cep'] = 'CEP inválido, digite os 8 digitos do CEP'
+
+        if error_messages:
+            raise ValidationError(error_messages)
 
     class Meta:
         verbose_name = 'Perfil'
