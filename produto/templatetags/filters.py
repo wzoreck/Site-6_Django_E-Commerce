88befb69f7d75f5e3f50cddfe552de9f4a0192d4
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-07-15 14:50:02 -0300

    Link para acessar o produto e exibindo quantos produtos tem no carrinho na nav

--- produto/templatetags/filters.py
+++ produto/templatetags/filters.py
@@ -5,3 +5,7 @@ register = Library()
 @register.filter
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+@register.filter
+def quantidade_total_carrinho(carrinho):
+    return sum([item['quantidade'] for item in carrinho.values()])
