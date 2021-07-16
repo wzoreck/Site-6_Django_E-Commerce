fdebc7a492dddde06fd3e644aa9ab5e873126a71
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-07-16 08:59:37 -0300

    Filtro criado para somar o total do preco produtos

--- produto/templatetags/filters.py
+++ produto/templatetags/filters.py
@@ -9,3 +9,15 @@ def formata_preco(val):
 @register.filter
 def quantidade_total_carrinho(carrinho):
     return sum([item['quantidade'] for item in carrinho.values()])
+
+@register.filter
+def preco_total_carrinho(carrinho):
+    return sum(
+        [
+            item.get('preco_quantitativo_promocional')
+            if item.get('preco_quantitativo_promocional')
+            else item.get('preco_quantitativo')
+            for item
+            in carrinho.values()
+        ]
+    )
