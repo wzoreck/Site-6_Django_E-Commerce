fd59fd5b957fd1cd455802243486053e4619e566
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-07-23 11:53:31 -0300

    Correcao de bugs

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from . import models
 from django.views import View
+from perfil.models import Perfil
 from django.contrib import messages
 from django.http import HttpResponse
 from django.views.generic.list import ListView
@@ -166,6 +167,22 @@ class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('perfil:criar')
+
+        perfil = Perfil.objects.filter(usuario=self.request.user).exists()
+
+        if not perfil:
+            messages.error(
+                self.request,
+                'É necessário criar um perfil antes'
+            )
+            return redirect('perfil:criar')
+        
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio'
+            )
+            return redirect('produto:lista')
 
         contexto = {
             'usuario': self.request.user,
