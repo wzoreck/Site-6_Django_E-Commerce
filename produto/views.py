e7354fecbaa0bf2ce219ff180b36f8bca17570a0
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-07-23 09:52:17 -0300

    Inicio pagina resumo da compra

--- produto/views.py
+++ produto/views.py
@@ -164,4 +164,13 @@ class Carrinho(View):
 
 class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('ResumoDaCompra')
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+
+        contexto = {
+            'usuario': self.request.user,
+            'carrinho': self.request.session['carrinho'],
+
+        }
+
+        return render(self.request, 'produto/resumo_compra.html', contexto)
